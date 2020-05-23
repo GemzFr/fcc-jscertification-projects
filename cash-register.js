38e77a088fa60e12b86adf47cc4e008f2aaa8229
Gilles Margerin <gillesmargerin@gmail.com>
2020-05-23 16:28:27 -0500

    minor code cleaning, adding comments

--- cash-register.js
+++ cash-register.js
@@ -5,7 +5,7 @@ argument (price), payment as the second argument (cash), and cash-in-drawer (cid
 */
 
 function checkCashRegister(price, cash, cid) {
-  const values = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100];
+  const values = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100];  //bill/coin values
   let change = cash - price;
   let cashInCid = cid.map(elem => elem[1]).reduce((a,b) => a+b);
   let result = {status: "OPEN", change: []}
@@ -18,19 +18,18 @@ function checkCashRegister(price, cash, cid) {
   
   if (cashInCid > change) {
   for (let i = values.length-1; i > -1; --i) {
-    let money = 0;
+    let money = 0;     //storing each type of bill/coin accumulated value
 
     while (values[i] <= change && cid[i][1] > 0) {
         change -= values[i];
-        change = Math.round(change*100)/100
+        change = Math.round(change*100)/100   //avoids minor math issues
         money += values[i];
         cid[i][1] -= values[i];
       }
     if (money) result["change"].push([cid[i][0], money]);
     }
-
-    let test = result["change"].reduce(function(sum, value) {
-    return sum + value[1];},0);
+    //if exact change can't be given due to insufficient bill/coin stack
+    let test = result["change"].reduce((sum, value) => sum + value[1],0); 
 
     if (test < change) {
       result.status = "INSUFFICIENT_FUNDS";
