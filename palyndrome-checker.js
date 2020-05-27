02ee1eba2551019e25546cfd5b8c4c76a6e2aa52
Gilles Margerin <gillesmargerin@gmail.com>
2020-05-27 11:34:45 -0500

    improved with early return

--- palyndrome-checker.js
+++ palyndrome-checker.js
@@ -7,14 +7,11 @@ forward and backward, ignoring punctuation, case, and spacing.*/
 //basic code, to improve
 
 function palindrome(str) {
-    let palCheck = str.toLowerCase().replace(/\W|_/g, "");
-    let lengthCheck = Math.round(palCheck.length/2)
-    let counter = 0;
-   
-    for (let i=0,j=palCheck.length-1;i<lengthCheck;i++,j--){
-      if (palCheck[i] == palCheck[j]) {
-          counter += 1;
-      };
-    }
-    return counter === lengthCheck? true : false;
+  let palCheck = str.toLowerCase().replace(/\W|_/g, "");
+  let lengthCheck = Math.round(palCheck.length/2);
+  
+  for (let i=0,j=palCheck.length-1;i<lengthCheck;i++,j--){
+    if (palCheck[i] !== palCheck[j]) return false;
   }
+  return true;
+}
